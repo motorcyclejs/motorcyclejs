ae797820dbdcdb3a5d236c9be7d977352ce5ef05
TylorS <tlsteinberger167@gmail.com>
2015-12-20 21:10:20 -0500

    test(perf): update performance test
    Update the performance test with better
    code. Makes things noticeably smoother.
    
    References #44

--- dom/test/browser/perf/index.js
+++ dom/test/browser/perf/index.js
@@ -3,107 +3,76 @@ import { makeDomDriver, input, div, h2 } from '../../../src'
 import most from 'most'
 import map from 'fast.js/array/map'
 
-const InputCount =
-  sources => {
-    const id = `.component-count`
-    const value$ =
-      sources.DOM
-        .select(id)
-        .events(`input`)
-        .map(ev => ev.target.value)
-        .merge(sources.value$)
+function InputCount(dom) {
+  const id = `.component-count`
+  const value$ = dom.select(id)
+      .events(`input`)
+      .map(ev => ev.target.value)
+      .startWith(100)
+      .skipRepeats()
 
-    return {
-      value$
-      , DOM: value$.map(
-        value => input(
-          `${id}`
-          , {
-              props: {
-                type: `range`
-                , max: `250`
-                , min: `0`
-                , value
-                ,
-              }
-            , style: {
-                width: `100%`
-                ,
-              }
-            ,
-          }
-        )
-      )
-    }
-  }
-
-const CycleJSLogo =
-  id => ({
-    DOM: most.just(
-      div({
-        style: {
-          alignItems: `center`
-          , background: `url(http://cycle.js.org/img/cyclejs_logo.svg)`
-          , boxSizing: `border-box`
-          , display: `inline-flex`
-          , fontFamily: `sans-serif`
-          , fontWeight: `700`
-          , fontSize: `8px`
-          , height: `32px`
-          , justifyContent: `center`
-          , margin: `8px`
-          , width: `32px`
-        }
+  return {
+    dom: value$.map(value => input(id, {
+      props: {
+        type: 'range',
+        max: 250,
+        min: 1,
+        value,
+      },
+      style: {
+        width: '100%'
       }
-      , [
-        id
-      ])
-    )
-  })
-
-const view =
-  (value, inputCountVTree, componentDOMs) =>
-    div([
-      h2([`# of Components: ${value}`])
-      , inputCountVTree
-      , div(componentDOMs)
-    ])
-
-const Main =
-  sources => {
-    const inputCount = InputCount({
-      DOM: sources.DOM
-      , value$: most.just(100)
-    })
-
-    const components$ =
-      inputCount
-        .value$
-        .map(
-          value =>
-          most.combine(
-            (...components) => components
-            , ...map(
-              Array(parseInt(value))
-              , (v, i) => CycleJSLogo(i+1).DOM
-            )
-          )
-        )
-          .switch()
-          .skipRepeats()
-
-    const view$ =
-      most.combine(
-        view
-        , inputCount.value$
-        , inputCount.DOM
-        , components$
-      )
-        .skipRepeats()
-
-    return {
-      DOM: view$
-    }
+    })),
+    value$
   }
+}
 
-run(Main, {DOM: makeDomDriver(`#mocha`)});
+function CycleJSLogo(id) {
+  return {
+    dom: most.just(div({
+      key: id,
+      style: {
+        alignItems: 'center',
+        background: 'url(./cyclejs_logo.svg)',
+        boxSizing: 'border-box',
+        display: 'inline-flex',
+        fontFamily: 'sans-serif',
+        fontWeight: '700',
+        fontSize: '8px',
+        height: '32px',
+        justifyContent: 'center',
+        margin: '8px',
+        width: '32px'
+      }
+    }, `${id}`))
+  }
+}
+
+function view(value, inputCountVTree, componentDOMs) {
+  return div([
+    h2([`# of Components: ${value}`]),
+    inputCountVTree,
+    div(componentDOMs)
+  ])
+}
+
+function Main(sources) {
+  const inputCount = InputCount(sources.dom)
+
+  const components$ = inputCount.value$
+    .map(value => most.combine(
+      (...components) => components,
+      ...map(Array(parseInt(value)), (v, i) => CycleJSLogo(i+1).dom)
+    ))
+    .switch()
+
+  const view$ =
+    most.combine(view, inputCount.value$, inputCount.dom, components$)
+
+  return {dom: view$}
+}
+
+run(Main, {dom: makeDomDriver(`#test-container`, [
+  require('snabbdom/modules/props'),
+  require('snabbdom/modules/style')
+])});
