fc9e2bcbbd2bfb7640c6f9ae53c72891140fc61f
TylorS <tlsteinberger167@gmail.com>
2015-12-29 11:30:47 -0500

    feat(events): Switch to event delegation
    This switches to using event delegation and filtering of the
    event source to allow things to continue to work under the same semantics, but greating
    reducing the cost of the operation.
    
    References #4

--- dom/src/events.js
+++ dom/src/events.js
@@ -1,22 +1,43 @@
 import {empty} from 'most'
 import fromEvent from './fromEvent'
+import {makeIsStrictlyInRootScope} from './select'
 
-const makeEventsSelector =
-  element$ =>
-    (type, useCapture = false) => {
-      if (typeof type !== `string`) {
-        throw new Error(`DOM drivers events() expects argument to be a ` +
-          `string representing the event type to listen for.`)
-      }
-      return element$
-        .map(
-          elements =>
-            elements ?
-              fromEvent(type, elements, useCapture) :
-              empty()
-        )
-        .switch()
-        .multicast()
+let matchesSelector
+try {
+  matchesSelector = require(`matches-selector`)
+} catch (err) {
+  matchesSelector = () => {}
+}
+
+function makeEventsSelector(element$, selector) {
+  return function eventsSelector(type, useCapture = false) {
+    if (typeof type !== `string`) {
+      throw new Error(`DOM drivers events() expects argument to be a ` +
+        `string representing the event type to listen for.`)
     }
+    return element$
+      .map(elements => {
+        if (!elements) {
+          return empty()
+        }
 
-export {makeEventsSelector}
+        if (matchesSelector(elements, selector.join(` `))) {
+          return fromEvent(type, elements, useCapture)
+        }
+
+        return fromEvent(type, elements, useCapture)
+          .filter(ev => {
+            if (matchesSelector(ev.srcElement, selector.join(` `)) ||
+              matchesSelector(ev.srcElement, selector.join(``)))
+            {
+              return makeIsStrictlyInRootScope(selector)(ev.srcElement)
+            }
+            return false
+          })
+      })
+      .switch()
+      .multicast()
+  }
+}
+
+export default makeEventsSelector
