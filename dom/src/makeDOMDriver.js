20b24fb6d408050d36f8513ee9254af6a4bc6519
TylorS <tlsteinberger167@gmail.com>
2015-12-29 11:40:29 -0500

    feat(makeDOMDriver): pass a stream of the rootElem to makeElementSelector
    This changes rootVNode$ to rootElement$ because the base of all of the
    changes require working with the rootElement. view$ is wrapped up in
    most.creat() again as well, and the reason for that is, without it, by the time
    the vNode.elm has reached other functions it is still undefined.

--- dom/src/makeDOMDriver.js
+++ dom/src/makeDOMDriver.js
@@ -1,3 +1,4 @@
+import {create} from 'most'
 import hold from '@most/hold'
 import snabbdom from 'snabbdom'
 import h from 'snabbdom/h'
@@ -7,30 +8,32 @@ import selectorParser from 'snabbdom-selector/lib/selectorParser'
 import {domSelectorParser} from './utils'
 import vTreeParser from './vTreeParser'
 import {isolateSink, isolateSource} from './isolate'
-import {makeSelectorParser} from './select'
+import makeElementSelector from './select'
 
-function vNodeWrapper(vNode, rootElement) {
-  const {tagName: selectorTagName, id: selectorId} = selectorParser(vNode.sel)
-  const vNodeClassName = classNameFromVNode(vNode)
-  const {data: vNodeData = {}} = vNode
-  const {props: vNodeDataProps = {}} = vNodeData
-  const {id: vNodeId = selectorId} = vNodeDataProps
+function makeVNodeWrapper(rootElement) {
+  return function vNodeWrapper(vNode) {
+    const {tagName: selectorTagName, id: selectorId} = selectorParser(vNode.sel)
+    const vNodeClassName = classNameFromVNode(vNode)
+    const {data: vNodeData = {}} = vNode
+    const {props: vNodeDataProps = {}} = vNodeData
+    const {id: vNodeId = selectorId} = vNodeDataProps
 
-  const isVNodeAndRootElementIdentical =
-    vNodeId === rootElement.id &&
-    selectorTagName === rootElement.tagName &&
-    vNodeClassName === rootElement.className
+    const isVNodeAndRootElementIdentical =
+      vNodeId === rootElement.id &&
+      selectorTagName === rootElement.tagName &&
+      vNodeClassName === rootElement.className
 
-  if (isVNodeAndRootElementIdentical) {
-    return vNode
+    if (isVNodeAndRootElementIdentical) {
+      return vNode
+    }
+
+    const {tagName, id, className} = rootElement
+    const elementId = id ? `#${id}` : ``
+    const elementClassName = className ?
+      `.${className.split(` `).join(`.`)}` :
+      ``
+    return h(`${tagName}${elementId}${elementClassName}`, {}, [vNode])
   }
-
-  const {tagName, id, className} = rootElement
-  const elementId = id ? `#${id}` : ``
-  const elementClassName = className ?
-    `.${className.split(` `).join(`.`)}` :
-    ``
-  return h(`${tagName}${elementId}${elementClassName}`, {}, [vNode])
 }
 
 const domDriverInputGuard =
@@ -55,20 +58,23 @@ const makeDOMDriver =
       view$ => {
         domDriverInputGuard(view$)
 
-        const rootVNode$ =
+        const rootElement$ = hold(create(add => {
           view$
             .map(vTreeParser)
             .switch()
-            .scan(
-              (prevVNode, vNode) =>
-                patch(prevVNode, vNodeWrapper(vNode, rootElement)),
-              rootElement
-            )
-            .skip(1)
+            .map(makeVNodeWrapper(rootElement))
+            .reduce((prev, curr) => {
+              const vNode = patch(prev, curr)
+              add(vNode.elm)
+              return vNode
+            }, rootElement)
+        }))
+
+        rootElement$.drain()
 
         return {
           namespace: [],
-          select: makeSelectorParser(hold(rootVNode$)),
+          select: makeElementSelector(rootElement$),
           isolateSink,
           isolateSource,
         }
