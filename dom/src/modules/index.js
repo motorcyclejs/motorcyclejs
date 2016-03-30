8296a157dec362f58a74dbb8890357663e5d6f35
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-03-30 00:42:05 -0400

    fix(): dataset module has not yet been publised to npm

--- dom/src/modules/index.js
+++ dom/src/modules/index.js
@@ -4,7 +4,6 @@ import AttrsModule from 'snabbdom/modules/attributes'
 import EventsModule from 'snabbdom/modules/eventlisteners'
 import StyleModule from 'snabbdom/modules/style'
 import HeroModule from 'snabbdom/modules/hero'
-import DataSetModule from 'snabbdom/modules/dataset'
 
 export default [StyleModule, ClassModule, PropsModule, AttrsModule]
 
@@ -12,5 +11,4 @@ export {
   StyleModule, ClassModule,
   PropsModule, AttrsModule,
   HeroModule, EventsModule,
-  DataSetModule,
 }
