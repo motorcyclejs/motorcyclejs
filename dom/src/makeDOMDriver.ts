50b17b2b8b57e8aabd7b49a84ae4f7da51750442
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-11-14 15:06:02 -0500

    fix(dom): fix errors around most.js Stream instances
    Remove checks for input to makeDOMDriver and DOMDriver in order to
    avoid incompatibilities of most.js version which technically are different
    instances of Stream. Also favor TypeScript for error handling than duck typing

--- dom/src/makeDOMDriver.ts
+++ dom/src/makeDOMDriver.ts
@@ -11,20 +11,6 @@ import { IsolateModule } from './modules/isolate'
 import { transposeVNode } from './transposition'
 import { EventDelegator } from './EventDelegator'
 
-function makeDOMDriverInputGuard (modules: any) {
-  if (!Array.isArray(modules)) {
-    throw new Error(`Optional modules option must be ` +
-     `an array for snabbdom modules`);
-  }
-}
-
-function domDriverInputGuard(view$: Stream<VNode>): void {
-  if (view$ instanceof Stream === false) {
-    throw new Error(`The DOM driver function expects as input a Stream of ` +
-      `virtual DOM elements`)
-  }
-}
-
 function mapVNodeToElement(vNode: VNode) {
   return vNode.elm ? vNode.elm as HTMLElement : vNode as any as HTMLElement
 }
@@ -43,10 +29,8 @@ export function makeDOMDriver(container: string | HTMLElement, options?: DOMDriv
   const rootElement = getElement(container);
   const vnodeWrapper = new VNodeWrapper(rootElement);
   const delegators = new Map<string, EventDelegator>();
-  makeDOMDriverInputGuard(modules);
 
   return function DOMDriver(vnode$: Stream<VNode>): DOMSource {
-    domDriverInputGuard(vnode$);
     const preprocessedVNode$ = transposition
       ? vnode$.map(transposeVNode).switch()
       : vnode$
