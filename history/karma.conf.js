53861fcbd65f426b1e13d104eeca083232a8bbee
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-11-17 00:28:55 -0500

    chore(travis): fixes for travis

--- history/karma.conf.js
+++ history/karma.conf.js
@@ -1,15 +1,28 @@
 module.exports = function (config) {
-  config.set({
-    files: [
-      'test/browser/bundle.js'
-    ],
+  const config =
+    {
+      files: [
+        'test/browser/bundle.js'
+      ],
 
-    frameworks: [
-      'mocha'
-    ],
+      frameworks: [
+        'mocha'
+      ],
 
-    browsers: [
-      'Chrome',
-    ],
-  });
+      customLaunchers: {
+        Chrome_travis_ci: {
+          base: 'Chrome',
+          flags: ['--no-sandbox']
+        }
+      },
+
+      browsers: [
+        'Chrome',
+      ],
+    }
+
+  if (process.env.TRAVIS)
+    config.browsers = ['Chrome_travis_ci'];
+
+  config.get(config);
 }
