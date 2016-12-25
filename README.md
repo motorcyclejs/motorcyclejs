a5fece2a04bb70a0c6017cb7151b0d951045ab0f
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-12-25 00:48:10 -0500

    chore(META): add monorepo configuration

---
+++ README.md
@@ -0,0 +1,14 @@
+# Motorcycle.js
+
+> Type-Safe Functional and Reactive Web Framework
+
+<!-- Write a short summary about your library here -->
+
+## Let me have it!
+```sh
+npm install --save Motorcycle.js
+```
+
+## API
+
+<!-- Describe your API here -->
---
+++ northbrook.ts
@@ -0,0 +1,31 @@
+import { plugin as mocha } from '@northbrook/mocha';
+import { plugin as tsc } from '@northbrook/tsc';
+import { plugin as tslint } from '@northbrook/tslint';
+import { plugin as northbrook } from 'northbrook/plugins';
+
+import { NorthbrookConfig } from 'northbrook';
+
+const config: NorthbrookConfig =
+  {
+    packages: [
+      'dom',
+      'history',
+      'router',
+      'run',
+    ],
+
+    plugins: [
+      northbrook,
+      mocha,
+      tsc,
+      tslint,
+    ],
+
+    tslint: {
+      patterns: [
+        'src/**/.ts',
+      ]
+    }
+  };
+
+export = config;
