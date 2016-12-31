d3d631226420cae25458a8c072cc49711e498c4a
Frederik Krautwald <fkrautwald@gmail.com>
2016-12-31 02:50:49 +0100

    feat(driver): add driver
    AFFECTS: @motorcycle/i18n
    
    BREAKING CHANGES: first implementation

--- i18n/src/makeI18nDriver.ts
+++ i18n/src/makeI18nDriver.ts
@@ -1,11 +1,33 @@
-import { Stream, just } from 'most';
+import * as i18next from 'i18next';
 
-export function makeI18nDriver() {
-  return function i18nDriver(language$: Stream<string>) {
-    Function.prototype(language$);
+import { Stream, map, startWith } from 'most';
 
-    return function i18Source(key: string): Stream<any> {
-      return just(key);
+import { sync } from 'most-subject';
+
+export type I18nSource =
+  (key: string, translationOptions?: i18next.TranslationOptions) => Stream<string>;
+
+export function makeI18nDriver(plugins: Array<any> = [], options?: i18next.Options) {
+
+  return function i18nDriver(sink$: Stream<string>): I18nSource {
+
+    const translationFn$ = sync<i18next.TranslationFunction>();
+
+    plugins
+      .reduce((i18n, plugin) => i18n.use(plugin), i18next)
+      .init(options, function () {
+        sink$.observe((language: string) => {
+          i18next.changeLanguage(language, function (err: any, t: i18next.TranslationFunction) {
+            if (err)
+              return translationFn$.error(err);
+
+            translationFn$.next(t);
+          });
+        });
+      });
+
+    return function i18nSource(key: string, translationOptions?: i18next.TranslationOptions) {
+      return map(t => t(key, translationOptions), translationFn$);
     };
   };
 }
