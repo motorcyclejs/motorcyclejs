5f14a6c60a8ff2f6f61162b2d27c6216ab34e39c
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-02-11 15:14:04 -0500

    fix(examples/todo-app): working svg content

--- examples/todo-app/src/ui/item/styles.ts
+++ examples/todo-app/src/ui/item/styles.ts
@@ -1,4 +1,4 @@
-import { style } from 'typestyle';
+import { cssRaw, style } from 'typestyle';
 
 export namespace ItemStyles {
   export const item = style(
@@ -7,10 +7,8 @@ export namespace ItemStyles {
       position: 'relative',
       fontSize: `24px`,
       borderBottom: `1px solid #ededed`,
-    }
+    },
   );
-
-  const toggleSvg = `url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="#ededed" stroke-width="3"/></svg>\')`;
 
   export const toggle = style(
     { $debugName: `todo-app_item-toggle` },
@@ -24,14 +22,15 @@ export namespace ItemStyles {
       margin: `auto 0`,
       '-webkit-appearance': `none`,
       appearance: 'none',
-      $nest: {
-        '&::after': {
-          content: `' '`,
-          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="#ededed" stroke-width="3"/></svg>')`,
-        },
-      },
-    }
+    },
   );
+
+  const toggleSvg = `url(\'data:image/svg+xml;utf8,` +
+    `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40"` +
+     ` viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" ` +
+     `stroke="#ededed" stroke-width="3"/></svg>\')`;
+
+  cssRaw(`.${toggle}::after { content: ${toggleSvg} }`);
 
   export const label = style(
     { $debugName: `todo-app_item-label`},
@@ -42,6 +41,6 @@ export namespace ItemStyles {
       display: `block`,
       lineHeight: 1.2,
       transition: `color 0.4s`,
-    }
+    },
   );
 }
