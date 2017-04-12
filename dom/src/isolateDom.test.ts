102437e47ea3f6ff9f6064f9baf77bd83bb972d0
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-04-12 09:32:44 -0400

    fix(isolateDom): generate scope key before calling component
    Generating a key when calling the isolated component can lead to
    inconsistent behavior, and errors when trying to patch the dom.
    
    AFFECTS: @motorcycle/dom
    
    BREAKING CHANGES: generate scope when calling isolateDom

---
+++ dom/src/isolateDom.test.ts
@@ -0,0 +1,32 @@
+import * as assert from 'assert';
+
+import { DomSinks, DomSources, makeDomComponent } from './makeDomComponent';
+
+import { MotorcycleDomSource } from './DomSources';
+import { div } from 'mostly-dom';
+import { isolateDom } from './isolateDom';
+import { just } from 'most';
+import { run } from '@motorcycle/run';
+
+const testElement = document.createElement('div');
+
+describe(`isolateDom`, () => {
+  it(`accepts an isolation scope `, (done) => {
+    const scope = `foo`;
+
+    function testComponent(sources: DomSources): DomSinks {
+      const namespace = sources.dom.namespace();
+
+      return { view$: just(div()) };
+    }
+
+    const dom = new MotorcycleDomSource(just(testElement), []);
+
+    const isolatedComponent = isolateDom(testComponent, scope);
+
+    isolatedComponent({ dom }).view$.observe((view) => {
+      assert.ok(view.scope && view.scope.indexOf(scope) > -1);
+      done();
+    });
+  });
+});
