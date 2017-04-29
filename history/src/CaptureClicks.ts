69290594332ed0861a4baa4b584538bba7167a94
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-04-28 20:47:03 -0400

    feat(history): upgrade to TS 2.3.x
    AFFECTS: @motorcycle/history

--- history/src/CaptureClicks.ts
+++ history/src/CaptureClicks.ts
@@ -1,88 +1,89 @@
-import { HistoryInput, HistorySinks, HistorySources, Path } from './';
-import { Subject, sync } from 'most-subject';
+import { HistoryInput, HistorySinks, HistorySources, Path } from './'
+import { Subject, sync } from 'most-subject'
 
-import { Stream } from 'most';
+import { Stream } from 'most'
 
 const clickEvent = 'undefined' !== typeof document && document.ontouchstart ?
-  'touchstart' : 'click';
+  'touchstart' : 'click'
 
 function which(ev: any) {
   if (typeof window === 'undefined') {
-    return false;
+    return false
   }
-  let e = ev || window.event;
-  return e.which === null ? e.button : e.which;
+  const e = ev || window.event
+
+  return e.which === null ? e.button : e.which
 }
 
 function sameOrigin(href: string) {
   if (typeof window === 'undefined') {
-    return false;
+    return false
   }
 
-  return href && href.indexOf(window.location.origin) === 0;
+  return href && href.indexOf(window.location.origin) === 0
 }
 
 function makeClickListener(push: Function) {
   return function clickListener(event: any) {
     if (which(event) !== 1) {
-      return;
+      return
     }
 
     if (event.metaKey || event.ctrlKey || event.shiftKey) {
-      return;
+      return
     }
 
     if (event.defaultPrevented) {
-      return;
+      return
     }
 
-    let element = event.target;
+    let element = event.target
     while (element && element.nodeName !== 'A') {
-      element = element.parentNode;
+      element = element.parentNode
     }
 
-    if (!element || element.nodeName !== 'A') { return; }
+    if (!element || element.nodeName !== 'A') { return }
 
     if (element.hasAttribute('download') ||
-      element.getAttribute('rel') === 'external') { return; }
+      element.getAttribute('rel') === 'external') { return }
 
-    if (element.target) { return; }
+    if (element.target) { return }
 
-    const link = element.getAttribute('href');
+    const link = element.getAttribute('href')
 
     if (link && link.indexOf('mailto:') > -1 || link.charAt(0) === '#') {
-      return;
+      return
     }
 
     if (!sameOrigin(element.href)) {
-      return;
+      return
     }
 
-    event.preventDefault();
-    const {pathname, search, hash = ''} = element;
-    push(pathname + search + hash);
-  };
+    event.preventDefault()
+    const { pathname, search, hash = '' } = element
+    push(pathname + search + hash)
+  }
 }
 
 function captureAnchorClicks(push: Function) {
-  const listener = makeClickListener(push);
+  const listener = makeClickListener(push)
 
   if (typeof window !== 'undefined')
-    document.addEventListener(clickEvent, listener, false);
+    document.addEventListener(clickEvent, listener, false)
 }
 
 export function CaptureClicks(
   History: (sinks: HistorySinks) => HistorySources)
 {
   return function historyDriverWithClickCapture(sinks: HistorySinks): HistorySources {
-    const subject: Subject<HistoryInput | Path> = sync<HistoryInput>();
+    const subject: Subject<HistoryInput | Path> = sync<HistoryInput>()
 
     captureAnchorClicks((path: string) => {
-      subject.next({ type: 'push', path });
-    });
+      subject.next({ type: 'push', path })
+    })
 
-    const stream: Stream<HistoryInput | Path> = subject.merge(sinks.history$);
+    const stream: Stream<HistoryInput | Path> = subject.merge(sinks.history$)
 
-    return History({ history$: stream });
-  };
+    return History({ history$: stream })
+  }
 }
