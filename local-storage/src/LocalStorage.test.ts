bf1abc5dd7c76621cbbbdc48e516c7b81e4025b5
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-04-28 20:59:16 -0400

    feat(local-storage): upgrade to TS 2.3.x
    AFFECTS: @motorcycle/local-storage

--- local-storage/src/LocalStorage.test.ts
+++ local-storage/src/LocalStorage.test.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:max-file-line-count */
-import * as assert from 'assert';
+import * as assert from 'assert'
 
 import {
   LocalStorage,
@@ -12,9 +12,9 @@ import {
   clear,
   removeItem,
   setItem,
-} from './';
-import { async, sync } from 'most-subject';
-import { just, never } from 'most';
+} from './'
+import { async, sync } from 'most-subject'
+import { just, never } from 'most'
 
 describe(`LocalStorage`, () => {
   describe(`given LocalStorageSinks`, () => {
@@ -22,42 +22,42 @@ describe(`LocalStorage`, () => {
       const sinks: LocalStorageSinks =
         {
           localStorage$: just(clear()),
-        };
+        }
 
-      const sources: LocalStorageSources = LocalStorage(sinks);
+      const sources: LocalStorageSources = LocalStorage(sinks)
 
-      const { localStorage } = sources;
+      const { localStorage } = sources
 
-      assert.strictEqual(typeof localStorage.getItem, 'function');
-    });
-  });
+      assert.strictEqual(typeof localStorage.getItem, 'function')
+    })
+  })
 
   describe(`clear`, () => {
     describe(`when called`, () => {
       it(`returns a LocalStorageClearCommand`, () => {
-        const clearCommand: LocalStorageClearCommand = clear();
+        const clearCommand: LocalStorageClearCommand = clear()
 
-        assert.strictEqual(clearCommand.method, 'clear');
-      });
-    });
+        assert.strictEqual(clearCommand.method, 'clear')
+      })
+    })
 
     describe(`when passed to LocalStorage component`, () => {
       before(() => {
-        window.localStorage.setItem('a', '1');
-        window.localStorage.setItem('b', '2');
-      });
+        window.localStorage.setItem('a', '1')
+        window.localStorage.setItem('b', '2')
+      })
 
       it(`clears localStorage keys and values`, () => {
-        const localStorage$ = sync<LocalStorageCommand>();
+        const localStorage$ = sync<LocalStorageCommand>()
 
-        LocalStorage({ localStorage$ });
+        LocalStorage({ localStorage$ })
 
-        localStorage$.next(clear());
+        localStorage$.next(clear())
 
-        assert.strictEqual(window.localStorage.length, 0);
-      });
-    });
-  });
+        assert.strictEqual(window.localStorage.length, 0)
+      })
+    })
+  })
 
   describe(`removeItem`, () => {
     describe(`when given a string representing a key`, () => {
@@ -65,49 +65,49 @@ describe(`LocalStorage`, () => {
         const sinks: LocalStorageSinks =
           {
             localStorage$: never(),
-          };
+          }
 
-        LocalStorage(sinks);
+        LocalStorage(sinks)
 
-        const command: LocalStorageRemoveItemCommand = removeItem('a');
+        const command: LocalStorageRemoveItemCommand = removeItem('a')
 
-        assert.strictEqual(command.method, 'removeItem');
-        assert.strictEqual(command.key, 'a');
+        assert.strictEqual(command.method, 'removeItem')
+        assert.strictEqual(command.key, 'a')
 
-        const command2: LocalStorageRemoveItemCommand = removeItem('b');
+        const command2: LocalStorageRemoveItemCommand = removeItem('b')
 
-        assert.strictEqual(command2.method, 'removeItem');
-        assert.strictEqual(command2.key, 'b');
-      });
-    });
+        assert.strictEqual(command2.method, 'removeItem')
+        assert.strictEqual(command2.key, 'b')
+      })
+    })
 
     describe(`when passed to LocalStorage component`, () => {
       before(() => {
-        window.localStorage.setItem('a', '1');
-        window.localStorage.setItem('b', '2');
-      });
+        window.localStorage.setItem('a', '1')
+        window.localStorage.setItem('b', '2')
+      })
 
       after(() => {
-        window.localStorage.clear();
-      });
+        window.localStorage.clear()
+      })
 
       it(`removes given key:value from localStorage`, () => {
-        const localStorage$ = sync<LocalStorageCommand>();
+        const localStorage$ = sync<LocalStorageCommand>()
 
         const sinks: LocalStorageSinks =
           {
             localStorage$,
-          };
+          }
 
-        LocalStorage(sinks);
+        LocalStorage(sinks)
 
-        localStorage$.next(removeItem(`a`));
+        localStorage$.next(removeItem(`a`))
 
-        assert.strictEqual(window.localStorage.length, 1);
-        assert.strictEqual(window.localStorage.getItem('a'), null);
-      });
-    });
-  });
+        assert.strictEqual(window.localStorage.length, 1)
+        assert.strictEqual(window.localStorage.getItem('a'), null)
+      })
+    })
+  })
 
   describe('setItem', () => {
     describe(`when given a string representing a key and a string representing value`, () => {
@@ -115,185 +115,185 @@ describe(`LocalStorage`, () => {
         const sinks: LocalStorageSinks =
           {
             localStorage$: never(),
-          };
+          }
 
-        LocalStorage(sinks);
+        LocalStorage(sinks)
 
-        const command: LocalStorageSetItemCommand = setItem('a', '1');
+        const command: LocalStorageSetItemCommand = setItem('a', '1')
 
-        assert.strictEqual(command.method, 'setItem');
-        assert.strictEqual(command.key, 'a');
-        assert.strictEqual(command.value, '1');
+        assert.strictEqual(command.method, 'setItem')
+        assert.strictEqual(command.key, 'a')
+        assert.strictEqual(command.value, '1')
 
-        const command2: LocalStorageSetItemCommand = setItem('b', '2');
+        const command2: LocalStorageSetItemCommand = setItem('b', '2')
 
-        assert.strictEqual(command2.method, 'setItem');
-        assert.strictEqual(command2.key, 'b');
-        assert.strictEqual(command2.value, '2');
-      });
-    });
+        assert.strictEqual(command2.method, 'setItem')
+        assert.strictEqual(command2.key, 'b')
+        assert.strictEqual(command2.value, '2')
+      })
+    })
 
     describe(`when passed to LocalStorage component`, () => {
       before(() => {
-        window.localStorage.clear();
-      });
+        window.localStorage.clear()
+      })
 
       after(() => {
-        window.localStorage.clear();
-      });
+        window.localStorage.clear()
+      })
 
       it(`adds given key:value to localStorage`, () => {
-        const localStorage$ = sync<LocalStorageCommand>();
+        const localStorage$ = sync<LocalStorageCommand>()
 
         const sinks: LocalStorageSinks =
           {
             localStorage$,
-          };
+          }
 
-        LocalStorage(sinks);
+        LocalStorage(sinks)
 
-        localStorage$.next(setItem(`a`, `1`));
+        localStorage$.next(setItem(`a`, `1`))
 
-        assert.strictEqual(window.localStorage.length, 1);
-        assert.strictEqual(window.localStorage.getItem(`a`), `1`);
-      });
-    });
-  });
+        assert.strictEqual(window.localStorage.length, 1)
+        assert.strictEqual(window.localStorage.getItem(`a`), `1`)
+      })
+    })
+  })
 
   describe(`LocalStorageSource`, () => {
     describe(`getItem`, () => {
       describe(`given a string representing a key`, () => {
         beforeEach(() => {
-          window.localStorage.clear();
-        });
+          window.localStorage.clear()
+        })
 
         it(`returns a stream of corresponding values`, () => {
-          const localStorage$ = async<LocalStorageCommand>();
+          const localStorage$ = async<LocalStorageCommand>()
 
           const sinks: LocalStorageSinks =
             {
               localStorage$,
-            };
+            }
 
-          const sources: LocalStorageSources = LocalStorage(sinks);
+          const sources: LocalStorageSources = LocalStorage(sinks)
 
-          const { localStorage } = sources;
+          const { localStorage } = sources
 
           const promise = localStorage
             .getItem(`a`)
             .take(2)
-            .reduce((a: Array<string>, b: string) => a.concat(b), []);
+            .reduce((a: Array<string>, b: string) => a.concat(b), [])
 
-          localStorage$.next(setItem('a', '1'));
+          localStorage$.next(setItem('a', '1'))
 
           return promise.then((values: Array<string>) => {
-            assert.deepEqual(values, [null, `1`]);
-          });
-        });
+            assert.deepEqual(values, [null, `1`])
+          })
+        })
 
         it(`returns a stream of updated values`, (done) => {
-          const localStorage$ = async<LocalStorageCommand>();
+          const localStorage$ = async<LocalStorageCommand>()
 
           const sinks: LocalStorageSinks =
             {
               localStorage$,
-            };
+            }
 
-          const sources: LocalStorageSources = LocalStorage(sinks);
+          const sources: LocalStorageSources = LocalStorage(sinks)
 
-          const { localStorage } = sources;
+          const { localStorage } = sources
 
           const a$ = localStorage
             .getItem(`a`)
-            .take(4);
+            .take(4)
 
-          const expected = [null, `1`, `2`, `3`];
+          const expected = [null, `1`, `2`, `3`]
 
-          a$.observe(value => {
-            assert.strictEqual(value, expected.shift());
+          a$.observe((value) => {
+            assert.strictEqual(value, expected.shift())
 
             if (expected.length === 0)
-              done();
-          });
+              done()
+          })
 
-          localStorage$.next(setItem(`a`, `1`));
-          localStorage$.next(setItem(`a`, `2`));
-          localStorage$.next(setItem(`a`, `3`));
-        });
+          localStorage$.next(setItem(`a`, `1`))
+          localStorage$.next(setItem(`a`, `2`))
+          localStorage$.next(setItem(`a`, `3`))
+        })
 
         it(`returns a stream of updated values from other windows`, (done) => {
-          const otherWindow = window.open();
+          const otherWindow = window.open()
 
           const sinks: LocalStorageSinks =
             {
               localStorage$: never(),
-            };
+            }
 
-          const sources: LocalStorageSources = LocalStorage(sinks);
+          const sources: LocalStorageSources = LocalStorage(sinks)
 
-          const { localStorage } = sources;
+          const { localStorage } = sources
 
           const a$ = localStorage
             .getItem(`a`)
-            .take(4);
+            .take(4)
 
-          const expected = [null, `1`, `2`, `3`];
+          const expected = [null, `1`, `2`, `3`]
 
-          a$.observe(value => {
-            assert.strictEqual(value, expected.shift());
+          a$.observe((value) => {
+            assert.strictEqual(value, expected.shift())
 
             if (expected.length === 0)
-              done();
-          });
+              done()
+          })
 
           function setNextValue(value: number) {
             return new Promise((resolve) => {
-              otherWindow.localStorage.setItem(`a`, String(value));
+              otherWindow.localStorage.setItem(`a`, String(value))
 
-              setTimeout(resolve, 10, value + 1);
-            });
+              setTimeout(resolve, 10, value + 1)
+            })
           }
 
           Promise.resolve(1)
             .then(setNextValue)
             .then(setNextValue)
-            .then(setNextValue);
-        });
-      });
-    });
+            .then(setNextValue)
+        })
+      })
+    })
 
     describe('length', () => {
       describe(`when called`, () => {
         beforeEach(() => {
-          window.localStorage.clear();
-        });
+          window.localStorage.clear()
+        })
 
         it(`returns a stream of localStorage length`, () => {
-          const localStorage$ = async<LocalStorageCommand>();
+          const localStorage$ = async<LocalStorageCommand>()
 
           const sinks: LocalStorageSinks =
             {
               localStorage$,
-            };
+            }
 
-          const sources: LocalStorageSources = LocalStorage(sinks);
+          const sources: LocalStorageSources = LocalStorage(sinks)
 
-          const { localStorage } = sources;
+          const { localStorage } = sources
 
           const promise = localStorage
             .length()
             .take(4)
-            .reduce((a: Array<number>, b: number) => a.concat(b), []);
+            .reduce((a: Array<number>, b: number) => a.concat(b), [])
 
-          localStorage$.next(setItem('a', '1'));
-          localStorage$.next(setItem(`b`, `2`));
-          localStorage$.next(removeItem(`a`));
+          localStorage$.next(setItem('a', '1'))
+          localStorage$.next(setItem(`b`, `2`))
+          localStorage$.next(removeItem(`a`))
 
           return promise.then((values: Array<number>) => {
-            assert.deepEqual(values, [0, 1, 2, 1]);
-          });
-        });
-      });
-    });
-  });
-});
+            assert.deepEqual(values, [0, 1, 2, 1])
+          })
+        })
+      })
+    })
+  })
+})
