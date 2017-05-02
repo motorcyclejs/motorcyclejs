54b440b7a6e67048889d7fedd5f0d60c6d711b21
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-05-02 18:18:11 -0400

    style(events): fix tslint errors

--- dom/src/api-wrappers/events.ts
+++ dom/src/api-wrappers/events.ts
@@ -4,7 +4,7 @@ import { DomSource, StandardEvents } from '../types'
 import { Stream } from 'most'
 
 export const events: EventsFn = curry2<StandardEvents, DomSource, Stream<Event>>(
-  function (eventType: StandardEvents, domSource: DomSource): Stream<Event> {
+  function(eventType: StandardEvents, domSource: DomSource): Stream<Event> {
     return domSource.events(eventType)
   },
 )
